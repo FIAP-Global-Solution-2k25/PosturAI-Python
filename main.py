e20ff54899d54278985d7e679ce786d28e3b274e
Ulisses Abreu <Ulisses.wrk@gmail.com>
2025-11-17 23:38:57 -0300

    feature: adiciona tratamento de erro com try e except no sistema

--- main.py
+++ main.py
@@ -140,7 +140,17 @@ pose = mp_pose.Pose(
     smooth_landmarks=True        # Remove tremidas, evita alguns falsos positivos e deixa mais estavel
 )
 
-cam = cv2.VideoCapture(0, cv2.CAP_DSHOW) # Abre a webcam principal
+try: # Tenta realizar a abertura da webcam
+    cam = cv2.VideoCapture(0, cv2.CAP_DSHOW) # Abre a webcam principal
+
+    if not cam.isOpened(): # Caso ela não tenha sido aberta, da um raise com uma mensagem educativa
+        raise Exception("Erro ao abrir a câmera.")
+    
+except Exception as e: # Qualquer outro problema exceto o da má abertura da câmera resultará em outro print
+    print("Falha ao inicializar webcam:", e)
+    exit()
+
+cam = cv2.VideoCapture(0, cv2.CAP_DSHOW) 
 cv2.setUseOptimized(True)                # Ativa otimizações do próprio OpenCV
 cv2.setNumThreads(4)                     # Começa a processar certas operações de forma assincrona, deixando também mais rapido o inicio
 
@@ -150,197 +160,215 @@ cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
 frame_index = 0  # Como os landmarks (marcacoes do corpo) comecam instaveis, ele nao mostra os primeiros (deixando mais rapido)
 
-# Chama a função que muda a imagem da janela do projeto
-definir_icone_janela("logo-posturAI.ico")
+try: # Tenta executar a tarefa
+    definir_icone_janela("logo-posturAI.ico") # Chama a função que muda a imagem da janela do projeto
+except Exception as e: # Se algo ocorrer, retorna uma mensagem
+    print("Erro ao definir ícone:", e)
 
-while True: # Toda aplicação de visão computacional roda em cima desse loop
+try:
+    while True: # Toda aplicação de visão computacional roda em cima desse loop
 
-    sucesso, frame = cam.read() 
-    # frame é a imagem atual
-    # sucesso retorna true se a captura funcionou
-    
-    if not sucesso: # Se não funcionar ela quebra o loop
-        continue
-
-    imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # O media pipe usa RGB e o OpenCV BGR, então aqui convertemos
-
-    results = pose.process(imgRGB) # Processa as poses usando a comversão de BGR pra RGB
-
-    # Indicadores de postura, sendo true pra posturas erradas (tortas mesmo)
-    postura_frente_ruim = False
-    postura_tras_ruim = False
-    postura_lateral_ruim = False
-    postura_distancia_ruim = False 
-
-    if results.pose_landmarks: # Achou o corpo pra demarcar?
-        landmarks = results.pose_landmarks.landmark # Sim, então podemos acessar os pontos
-
-        #============= Projeção da cabeça pra frente ============
-        cabeca = landmarks[mp_pose.PoseLandmark.NOSE] # Pega a cabeça (nariz)
-        orelha_esq = landmarks[mp_pose.PoseLandmark.LEFT_EAR] # Pega a orelha esquerda
-        orelha_dir = landmarks[mp_pose.PoseLandmark.RIGHT_EAR] # Orelha direita
-
-        # Calculamos a diferença do eixo y da cabeca (na verdade do nariz) com a média das orelhas
-        diferenca_y = cabeca.y - ((orelha_esq.y + orelha_dir.y) / 2)
-
-        # Comparamos o resultado anterior com a sensibilidade que definimos 
-        # Que acima dele, podemos dizer que a postura esta torta
-        cabeca_inclinada_por_angulo = diferenca_y > SENSIBILIDADE_PROJ_FRONTAL
-
-        # Se o resultado acima der true, ele define a variável de postura como ruim
-        if cabeca_inclinada_por_angulo:
-            postura_frente_ruim = True
-
-        #======== Projeção da cabeça pra trás ================
-        # A mais dificil de todas, tentei de inúmeras maneiras e essa foi a que mais trouxe resultado
-        
-        # Tiramos a média da altura das orelhas
-        media_orelhas = ((orelha_esq.y + orelha_dir.y) / 2)
-
-        # Se a cabeça ta um pouco acima das orelhas então a pessoa ta inclinada
-        inclinacao_y_tras = cabeca.y < media_orelhas - 0.001 # Quanto menores os valores mais pra cima
-
-        # Pegamos as landmarks dos ombros (os pontos que marcam eles)
-        ombro_esq = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER]
-        ombro_dir = landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER]
-
-        # Quando a cabeça estiver mais pra frente que qualquer um dos ombros retorna true
-        inclinacao_z_tras = (
-            cabeca.z > ombro_esq.z + 0.005 or
-            cabeca.z > ombro_dir.z + 0.005
-        )
-
-        # Definimos as landmarks dos quadris
-        quadril_esq = landmarks[mp_pose.PoseLandmark.LEFT_HIP]
-        quadril_dir = landmarks[mp_pose.PoseLandmark.RIGHT_HIP]
-
-        # Função trabalhosa demais, ela calcula o angulo formado entre a, b e c (cabeca, orelha, quadril)
-        def angulo(a, b, c):
-
-            #   a
-            #     \
-            #      b        
-            #     /
-            #    c
-
-            ab = (a.x - b.x, a.y - b.y) # Pega o vetor AB do angulo
-            cb = (c.x - b.x, c.y - b.y) # Pega o vetor CB do angulo
-
-            produto_escalar = ab[0] * cb[0] + ab[1] * cb[1] # Calculo pra ver o quanto eles apontam pra mesma direção
-
-            mod_ab = math.sqrt(ab[0]**2 + ab[1]**2) # Tamanho do vetor AB (Módulo AB)
-            mod_cb = math.sqrt(cb[0]**2 + cb[1]**2) # Tamanho do vetor CB (Módulo CB)
-
-            if mod_ab * mod_cb == 0: # Se eles tiverem angulo zero, não da pra calcular o angulo
-                return 180
+        try:
+            sucesso, frame = cam.read() 
+            # frame é a imagem atual
+            # sucesso retorna true se a captura funcionou
             
-            cosseno = produto_escalar / (mod_ab * mod_cb) # Cosseno do angulo B
-            cosseno = min(1, max(-1, cosseno)) # Limita o valor pra arredondar bonitinho
-            return math.degrees(math.acos(cosseno)) # Converte em gráus e retorna
+            if not sucesso: # Se não funcionar ela quebra o loop
+                continue
+
+        except Exception as e: # Caso ocorra algo inesperado, retorna um aviso
+            print("Erro ao capturar frame:", e)
+            continue
+
+        imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # O media pipe usa RGB e o OpenCV BGR, então aqui convertemos
+
+        try:
+            results = pose.process(imgRGB) # Processa as poses usando a comversão de BGR pra RGB
+        except Exception as e:
+            print("Erro no processamento MediaPipe:", e)
+            continue
+
+        # Indicadores de postura, sendo true pra posturas erradas (tortas mesmo)
+        postura_frente_ruim = False
+        postura_tras_ruim = False
+        postura_lateral_ruim = False
+        postura_distancia_ruim = False 
+
+        if results.pose_landmarks: # Achou o corpo pra demarcar?
+            landmarks = results.pose_landmarks.landmark # Sim, então podemos acessar os pontos
+
+            #============= Projeção da cabeça pra frente ============
+            cabeca = landmarks[mp_pose.PoseLandmark.NOSE] # Pega a cabeça (nariz)
+            orelha_esq = landmarks[mp_pose.PoseLandmark.LEFT_EAR] # Pega a orelha esquerda
+            orelha_dir = landmarks[mp_pose.PoseLandmark.RIGHT_EAR] # Orelha direita
+
+            # Calculamos a diferença do eixo y da cabeca (na verdade do nariz) com a média das orelhas
+            diferenca_y = cabeca.y - ((orelha_esq.y + orelha_dir.y) / 2)
+
+            # Comparamos o resultado anterior com a sensibilidade que definimos 
+            # Que acima dele, podemos dizer que a postura esta torta
+            cabeca_inclinada_por_angulo = diferenca_y > SENSIBILIDADE_PROJ_FRONTAL
+
+            # Se o resultado acima der true, ele define a variável de postura como ruim
+            if cabeca_inclinada_por_angulo:
+                postura_frente_ruim = True
+
+            #======== Projeção da cabeça pra trás ================
+            # A mais dificil de todas, tentei de inúmeras maneiras e essa foi a que mais trouxe resultado
+            
+            # Tiramos a média da altura das orelhas
+            media_orelhas = ((orelha_esq.y + orelha_dir.y) / 2)
+
+            # Se a cabeça ta um pouco acima das orelhas então a pessoa ta inclinada
+            inclinacao_y_tras = cabeca.y < media_orelhas - 0.001 # Quanto menores os valores mais pra cima
+
+            # Pegamos as landmarks dos ombros (os pontos que marcam eles)
+            ombro_esq = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER]
+            ombro_dir = landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER]
+
+            # Quando a cabeça estiver mais pra frente que qualquer um dos ombros retorna true
+            inclinacao_z_tras = (
+                cabeca.z > ombro_esq.z + 0.005 or
+                cabeca.z > ombro_dir.z + 0.005
+            )
+
+            # Definimos as landmarks dos quadris
+            quadril_esq = landmarks[mp_pose.PoseLandmark.LEFT_HIP]
+            quadril_dir = landmarks[mp_pose.PoseLandmark.RIGHT_HIP]
+
+            # Função trabalhosa demais, ela calcula o angulo formado entre a, b e c (cabeca, orelha, quadril)
+            def angulo(a, b, c):
+
+                #   a
+                #     \
+                #      b        
+                #     /
+                #    c
+
+                ab = (a.x - b.x, a.y - b.y) # Pega o vetor AB do angulo
+                cb = (c.x - b.x, c.y - b.y) # Pega o vetor CB do angulo
+
+                produto_escalar = ab[0] * cb[0] + ab[1] * cb[1] # Calculo pra ver o quanto eles apontam pra mesma direção
+
+                mod_ab = math.sqrt(ab[0]**2 + ab[1]**2) # Tamanho do vetor AB (Módulo AB)
+                mod_cb = math.sqrt(cb[0]**2 + cb[1]**2) # Tamanho do vetor CB (Módulo CB)
+
+                if mod_ab * mod_cb == 0: # Se eles tiverem angulo zero, não da pra calcular o angulo
+                    return 180
+                
+                cosseno = produto_escalar / (mod_ab * mod_cb) # Cosseno do angulo B
+                cosseno = min(1, max(-1, cosseno)) # Limita o valor pra arredondar bonitinho
+                return math.degrees(math.acos(cosseno)) # Converte em gráus e retorna
 
 
-        ang_esq = angulo(cabeca, orelha_esq, quadril_esq) # Pega o angulo entre a cabeca, orelha esq e quadril esq
-        ang_dir = angulo(cabeca, orelha_dir, quadril_dir) # Pega o angulo entre a cabeca, orelha dir e quadril dir
+            ang_esq = angulo(cabeca, orelha_esq, quadril_esq) # Pega o angulo entre a cabeca, orelha esq e quadril esq
+            ang_dir = angulo(cabeca, orelha_dir, quadril_dir) # Pega o angulo entre a cabeca, orelha dir e quadril dir
 
-        # Se qualquer um dos lados estiver acima de 160 gráus, quer dizer que a cabeça ta muito alinhada com o tronco
-        # Normalmente mostra que está inclinado pra trás
-        inclinacao_angulo_tras = (ang_esq > 160 or ang_dir > 160)
+            # Se qualquer um dos lados estiver acima de 160 gráus, quer dizer que a cabeça ta muito alinhada com o tronco
+            # Normalmente mostra que está inclinado pra trás
+            inclinacao_angulo_tras = (ang_esq > 160 or ang_dir > 160)
 
-        # Se qualquer uma das condicionais que indicam se tem alguma variação na sua postura forem True, você está torto
-        cabeca_inclinada_para_tras_frame = (
-            inclinacao_y_tras or
-            inclinacao_z_tras or
-            inclinacao_angulo_tras
-        )
-        # Pega a média dos frames da lista de historico 
-        media_tras = atualizar_e_media(
-            historico_tras,
-            1 if cabeca_inclinada_para_tras_frame else 0,
-            TAMANHO_JANELA_TRAS
-        )
+            # Se qualquer uma das condicionais que indicam se tem alguma variação na sua postura forem True, você está torto
+            cabeca_inclinada_para_tras_frame = (
+                inclinacao_y_tras or
+                inclinacao_z_tras or
+                inclinacao_angulo_tras
+            )
+            # Pega a média dos frames da lista de historico 
+            media_tras = atualizar_e_media(
+                historico_tras,
+                1 if cabeca_inclinada_para_tras_frame else 0,
+                TAMANHO_JANELA_TRAS
+            )
 
-        # Se a média dos ultimos frames forem maiores que o valor 0.8 são ditos como inclinados
-        cabeca_inclinada_para_tras_estavel = media_tras > 0.08
+            # Se a média dos ultimos frames forem maiores que o valor 0.8 são ditos como inclinados
+            cabeca_inclinada_para_tras_estavel = media_tras > 0.08
 
-        # Atribui a variavel postura_tras_ruim como True 
-        if cabeca_inclinada_para_tras_estavel:
-            postura_tras_ruim = True
+            # Atribui a variavel postura_tras_ruim como True 
+            if cabeca_inclinada_para_tras_estavel:
+                postura_tras_ruim = True
 
-        # ========= Projeção lateral de cabeça ================
+            # ========= Projeção lateral de cabeça ================
 
-        # Calcula a diferença entre a altura das orelhas (somente valores positivos por conta do abs)
-        dif_lateral = abs(orelha_esq.y - orelha_dir.y)
+            # Calcula a diferença entre a altura das orelhas (somente valores positivos por conta do abs)
+            dif_lateral = abs(orelha_esq.y - orelha_dir.y)
 
-        # Compara o dif_lateral com a sensibilidade que pre-definimos no inicio do sistema
-        cabeca_inclinada_lateral = dif_lateral > SENSIBILIDADE_PROJ_LATERAL
+            # Compara o dif_lateral com a sensibilidade que pre-definimos no inicio do sistema
+            cabeca_inclinada_lateral = dif_lateral > SENSIBILIDADE_PROJ_LATERAL
 
-        # Atualiza a lista de historico de acordo com o tamanho maximo e faz a média entre os frames tortos e normais
-        media_lateral = atualizar_e_media(
-            historico_lateral,
-            1 if cabeca_inclinada_lateral else 0,
-            TAMANHO_JANELA_LATERAL
-        )
+            # Atualiza a lista de historico de acordo com o tamanho maximo e faz a média entre os frames tortos e normais
+            media_lateral = atualizar_e_media(
+                historico_lateral,
+                1 if cabeca_inclinada_lateral else 0,
+                TAMANHO_JANELA_LATERAL
+            )
 
-        # Caso a média dos frames sejam maiores que 0.6, eles estão inclinados
-        cabeca_lateral_estavel = media_lateral > 0.6
+            # Caso a média dos frames sejam maiores que 0.6, eles estão inclinados
+            cabeca_lateral_estavel = media_lateral > 0.6
 
-        # Caso seja true, atribui a variavel o valor
-        if cabeca_lateral_estavel:
-            postura_lateral_ruim = True
+            # Caso seja true, atribui a variavel o valor
+            if cabeca_lateral_estavel:
+                postura_lateral_ruim = True
 
-        # ========== Calculo da distância do usuário ==============
+            # ========== Calculo da distância do usuário ==============
 
-        # Calcula a diferença no eixo x (horizontal) entre as orelhas dir e esq
-        dist_orelhas = abs(orelha_esq.x - orelha_dir.x)
+            # Calcula a diferença no eixo x (horizontal) entre as orelhas dir e esq
+            dist_orelhas = abs(orelha_esq.x - orelha_dir.x)
 
-        # Caso a dist_orelhas seja menor que 0.15, o user ta muito longe do dispositivo
-        if dist_orelhas < 0.15:
-            postura_distancia_ruim = True
+            # Caso a dist_orelhas seja menor que 0.15, o user ta muito longe do dispositivo
+            if dist_orelhas < 0.15:
+                postura_distancia_ruim = True
 
-        # Se a cabeça estiver inclinada pra frente é atribuida a contagem na variavel de contagem
-        if cabeca_inclinada_por_angulo: 
-            contador_postura_ruim += 1
-        # Caso contrario, limpamos o histórico
+            # Se a cabeça estiver inclinada pra frente é atribuida a contagem na variavel de contagem
+            if cabeca_inclinada_por_angulo: 
+                contador_postura_ruim += 1
+            # Caso contrario, limpamos o histórico
+            else:
+                contador_postura_ruim = 0  
+                historico_frontal.clear()
+
+            # Só da alerta de instabilidade quando as ocorrencias de postura correspondam a quantidade de frames
+            # Que definimos para diferenciar uma postura boa ou ruim
+            cabeca_final_estavel = contador_postura_ruim >= LIMIAR_FRAMES_FRONTAL
+
+            # Só depois disso tudo afirmamos que a postura está de fato ruim (pra projeções frontais)
+            if cabeca_final_estavel:
+                postura_frente_ruim = True
+
+        # Se ele não achar um corpo pra identificar as partes ele limpa o histórico tbm
         else:
-            contador_postura_ruim = 0  
-            historico_frontal.clear()
+            contador_postura_ruim = 0
+            limpar_historicos()
 
-        # Só da alerta de instabilidade quando as ocorrencias de postura correspondam a quantidade de frames
-        # Que definimos para diferenciar uma postura boa ou ruim
-        cabeca_final_estavel = contador_postura_ruim >= LIMIAR_FRAMES_FRONTAL
+        # Se os valores do dicionario forem True, vão ser coletados pra serem printados em tela
+        status_postura = {
+            "frente": postura_frente_ruim,
+            "tras": postura_tras_ruim,
+            "lateral": postura_lateral_ruim,
+            "distancia": postura_distancia_ruim
+        }
 
-        # Só depois disso tudo afirmamos que a postura está de fato ruim (pra projeções frontais)
-        if cabeca_final_estavel:
-            postura_frente_ruim = True
+        try:
+            desenhar_barra_status(frame, status_postura) # Função que coleta o status_postura e caso sejam True, são printados
 
-    # Se ele não achar um corpo pra identificar as partes ele limpa o histórico tbm
-    else:
-        contador_postura_ruim = 0
-        limpar_historicos()
+        except Exception as e: # Exibe mensagem de erro caso algo dê errado
+            print("Erro ao desenhar a barra:", e)
 
-    # Se os valores do dicionario forem True, vão ser coletados pra serem printados em tela
-    status_postura = {
-        "frente": postura_frente_ruim,
-        "tras": postura_tras_ruim,
-        "lateral": postura_lateral_ruim,
-        "distancia": postura_distancia_ruim
-    }
+        # Nome da aba do windows que abre junto com a câmera
+        cv2.imshow("PosturAI - Camera", frame)
 
-    # Função que coleta o status_postura e caso sejam valores True, são printados
-    desenhar_barra_status(frame, status_postura)
+        # Always on top ativo
+        always_on_top()
 
-    # Nome da aba do windows que abre junto com a câmera
-    cv2.imshow("PosturAI - Camera", frame)
+        # Contador de frames processados (útil para debug)
+        frame_index += 1
 
-    # Always on top ativo
-    always_on_top()
+        # Caso o usuário aperte a tecla q ele quita do sistema
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    # Contador de frames processados (útil para debug)
-    frame_index += 1
-
-    # Caso o usuário aperte a tecla q ele quita do sistema
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+except Exception as e: # Se o loop inteiro der algum problema ele da um aviso
+    print("Erro inesperado no loop:", e)
 
 # Fecha a webcam
 cam.release()
